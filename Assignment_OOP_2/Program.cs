3b1bc07e1b8be224bae70fea72025e10c008d875
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-23 14:12:45 +0300

    Question 1 B

--- Assignment_OOP_2/Program.cs
+++ Assignment_OOP_2/Program.cs
@@ -12,6 +12,7 @@ namespace Assignment_OOP_2
         {
             //Part 01 : Theoretical Questions
             #region Question 1
+            // Question A
             /*
              Class:
             1- A reference type. 2- Can inherit from another class. 3- impliment in Big Projects.
@@ -19,7 +20,18 @@ namespace Assignment_OOP_2
             Struct:
             1- A value type. 2- Cannot inherit from another class or struct. 3- impliment in Small Projects because it have 14 Bytes to be good performance.
              */
+
+
+            //Question B
+
+            /*
+             Classes are generally more suitable for complex, 
+            interconnected objects in large applications, while structs are better suited for small, 
+            independent values that do not require inheritance or shared identity.
+             */
             #endregion
+            
+
         }
     }
 }
