dcb26bc351a6bd55184de5a63ad68ec48f423f44
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-23 14:07:43 +0300

    Question 1

--- Assignment_OOP_2/Program.cs
+++ Assignment_OOP_2/Program.cs
@@ -10,6 +10,16 @@ namespace Assignment_OOP_2
     {
         static void Main(string[] args)
         {
+            //Part 01 : Theoretical Questions
+            #region Question 1
+            /*
+             Class:
+            1- A reference type. 2- Can inherit from another class. 3- impliment in Big Projects.
+
+            Struct:
+            1- A value type. 2- Cannot inherit from another class or struct. 3- impliment in Small Projects because it have 14 Bytes to be good performance.
+             */
+            #endregion
         }
     }
 }
