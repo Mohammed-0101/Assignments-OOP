82c56ed74fcd6ca951a264bf32ba7f81e5b2f56b
Mohammed-0101 <m.h.gaafar333@gmail.com>
2026-09-23 14:23:11 +0300

    Question 2

--- Assignment_OOP_2/Program.cs
+++ Assignment_OOP_2/Program.cs
@@ -29,7 +29,20 @@ namespace Assignment_OOP_2
              */
             #endregion
 
-           
+            #region Question 2
+            // a) Which class is the parent class?
+            // Shipment
+
+            //b) Which class is the child class?
+            // ExpressShipment
+
+            //c) What members are inherited by ExpressShipment?
+            // TrackingCode
+
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+            //Inheritance reduces code duplication, improves maintainability,
+            //and makes large C# applications easier to develop and manage.
+            #endregion
         }
     }
 }
